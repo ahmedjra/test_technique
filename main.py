b00f747fe9794eaaa5e672ab6ac6deb2e29a773a
Octopus <jradi.ahmed2000@gmail.com>
2023-08-29 19:04:41 +0200

    main

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from preprocessing import preprocessing
 from evaluation import evaluation
+import os
 
 if __name__ == "__main__":
     preprocessing = preprocessing()
-    for name in os:
+    for name in os.listdir("/home/octopus/Downloads/test_technique_stage_NLP-OCR-20230727T133531Z-001/test_technique_stage_NLP-OCR/test"):
         preprocessing.main(name)
         evaluation.main(name)
