7d8d69722785903ba8d2ec0f2a781d19584495a9
Octopus <jradi.ahmed2000@gmail.com>
2023-08-29 20:25:30 +0200

    [UP] update code to remove print statements/home/octopus/anaconda3/bin/python

--- evaluation.py
+++ evaluation.py
@@ -15,3 +15,11 @@ class evaluation:
             completeness[column] = (non_missing_rows / total_rows) * 100
 
         return completeness
+    
+    def hist(df):
+        missing_values = dataf.isnull().sum()
+        missing_values.plot(kind='bar')
+        plt.title('Missing Values per Column')
+        plt.ylabel('Number of Missing Values')
+        plt.xlabel('Columns')
+        plt.show()
